e0d638030d82308d5be34be71bc8632950047670
neyadrian <neyadrian2018@gmail.com>
2026-03-22 14:31:57 -0300

    adicionando funcionários no main

--- src/Sistema/Main.java
+++ src/Sistema/Main.java
@@ -1,17 +1,21 @@
 package Sistema;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Model.Funcionario;
+import Model.Gerente;
+import Enum.Departamento;
+import Model.Vendedor;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ArrayList<Funcionario> listaFuncionarios = new ArrayList<>();
+
+        Funcionario g1 = new Gerente("Ney", 8000, Departamento.FINANCEIRO, 2000);
+        Funcionario g2 = new Gerente("Priscila", 9000, Departamento.RH, 2500);
+        Funcionario v1 = new Vendedor("Aelcio", 4000, Departamento.VENDAS, 1500);
+        Funcionario v2 = new Vendedor("Evely", 3500, Departamento.VENDAS, 1200);
+
     }
 }
