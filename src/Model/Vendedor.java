9c85d57547a19a50af4685d14296456871302f84
neyadrian <neyadrian2018@gmail.com>
2026-03-22 14:23:25 -0300

    construtor e get comissão em subclasse vendedor

--- src/Model/Vendedor.java
+++ src/Model/Vendedor.java
@@ -1,4 +1,19 @@
 package Model;
+import Enum.Departamento;
 
-public class Vendedor {
+public class Vendedor extends Funcionario{
+    private double comissao;
+
+    public Vendedor(String nome, double salarioBase, Departamento departamento, double comissao) {
+        super(nome, salarioBase, departamento);
+        this.comissao = comissao;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
 }
